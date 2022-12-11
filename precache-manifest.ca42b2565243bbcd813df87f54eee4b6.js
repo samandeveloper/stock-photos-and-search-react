65fe5509815b3edeb8a2c721a0e2a0e8c51873cb
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-11 13:58:39 -0700

    Updates

---
+++ precache-manifest.ca42b2565243bbcd813df87f54eee4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828df31f8ad93014ad500b677496c493",
+    "url": "/stock-photos-and-search-react/index.html"
+  },
+  {
+    "revision": "80bef149337b12b53a7e",
+    "url": "/stock-photos-and-search-react/static/css/main.e58eea65.chunk.css"
+  },
+  {
+    "revision": "833aaddd2aad21d82868",
+    "url": "/stock-photos-and-search-react/static/js/2.f9fac789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-photos-and-search-react/static/js/2.f9fac789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80bef149337b12b53a7e",
+    "url": "/stock-photos-and-search-react/static/js/main.a9ebdb7d.chunk.js"
+  },
+  {
+    "revision": "4a0937e9aba2bfe9aca4",
+    "url": "/stock-photos-and-search-react/static/js/runtime-main.f61c7c04.js"
+  }
+]);
